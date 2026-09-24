6c4bdb3b6105d0dc01f271893ed7bee303f74ba4
ajakiuwn <azakiunwanullah8@gmail.com>
2026-09-24 11:31:41 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,14 @@
+
+percentage = float(input("Enter the student's percentage: "))
+
+# Evaluate performance based on criteria
+if percentage >= 90:
+    print("Excellent performance")
+elif percentage >= 80:
+    print("Very Good performance")
+elif percentage >= 70:
+    print("Good performance")
+elif percentage >= 60:
+    print("average performance")
+else:
+    print("Below average performance")
